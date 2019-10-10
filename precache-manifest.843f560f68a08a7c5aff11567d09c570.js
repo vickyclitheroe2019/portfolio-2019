60a22ddd6121c40b55b1f11072aa83dc870bb817
Vicky Clitheroe <vickycandbrianm@gmail.com>
2019-10-09 22:47:13 -0400

    Updates

---
+++ precache-manifest.843f560f68a08a7c5aff11567d09c570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e5a26247493101168dfcb7eb72b992",
+    "url": "/portfolio-2019/index.html"
+  },
+  {
+    "revision": "0b3fdf391e86f18c4e88",
+    "url": "/portfolio-2019/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8a63d0c69e9ab9b8b70f",
+    "url": "/portfolio-2019/static/js/2.1632cf1e.chunk.js"
+  },
+  {
+    "revision": "0b3fdf391e86f18c4e88",
+    "url": "/portfolio-2019/static/js/main.702101f5.chunk.js"
+  },
+  {
+    "revision": "1a28eb1902b67a467183",
+    "url": "/portfolio-2019/static/js/runtime-main.50b32653.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio-2019/static/media/logo.25bf045c.svg"
+  }
+]);
